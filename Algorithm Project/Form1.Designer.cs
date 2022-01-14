8f2a071787e8a3ccb7d8720645426e735ab0ef11
Mohammed Elsabaa <mhmdelsabaa20@gmail.com>
2022-01-15 01:02:19 +0200

    commit_1

---
+++ Algorithm Project/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Algorithm_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_options = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.txt_show = new System.Windows.Forms.TextBox();
+            this.txt_file = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_output_path = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_options
+            // 
+            this.cb_options.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cb_options.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cb_options.FormattingEnabled = true;
+            this.cb_options.Location = new System.Drawing.Point(12, 61);
+            this.cb_options.Name = "cb_options";
+            this.cb_options.Size = new System.Drawing.Size(370, 24);
+            this.cb_options.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(370, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(12, 105);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(370, 24);
+            this.txt_input.TabIndex = 2;
+            this.txt_input.Text = "write number to search";
+            // 
+            // txt_show
+            // 
+            this.txt_show.Location = new System.Drawing.Point(12, 154);
+            this.txt_show.Name = "txt_show";
+            this.txt_show.Size = new System.Drawing.Size(370, 24);
+            this.txt_show.TabIndex = 3;
+            // 
+            // txt_file
+            // 
+            this.txt_file.Location = new System.Drawing.Point(12, 13);
+            this.txt_file.Name = "txt_file";
+            this.txt_file.Size = new System.Drawing.Size(370, 24);
+            this.txt_file.TabIndex = 4;
+            this.txt_file.Text = "write path of file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(370, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "output of sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_output_path
+            // 
+            this.txt_output_path.Location = new System.Drawing.Point(0, 14);
+            this.txt_output_path.Name = "txt_output_path";
+            this.txt_output_path.Size = new System.Drawing.Size(370, 24);
+            this.txt_output_path.TabIndex = 6;
+            this.txt_output_path.Text = "write path of  output file";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_output_path);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(12, 257);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(370, 100);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 369);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txt_file);
+            this.Controls.Add(this.txt_show);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cb_options);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_options;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.TextBox txt_show;
+        private System.Windows.Forms.TextBox txt_file;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_output_path;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Algorithm Project/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Algorithm_Project
+{
+    
+    public partial class Form1 : Form
+    {
+        string str="";
+        int[] arr;
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb_options.Items.Add("Read File");
+            cb_options.Items.Add("Write File");
+            cb_options.Items.Add("merge sort file");
+            cb_options.Items.Add("quik sort file");
+            cb_options.Items.Add("heap sort file");
+            cb_options.Items.Add("counting sort");
+            cb_options.Items.Add("search for number");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            
+            if (cb_options.SelectedItem == "Read File")
+            {
+                if (str != "")
+                    str = "";
+                try
+                {
+                    StreamReader f = new StreamReader(txt_file.Text);
+                    while ((line = f.ReadLine()) != null)
+                    {
+                        str += line;
+                        str += Environment.NewLine;
+                    }
+                    f.Close();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("enter valid file path");
+                }
+                
+            }
+            else if (cb_options.SelectedItem == "Write File")
+            {
+                StreamWriter f = new StreamWriter("write_test.txt");
+                f.Write(str);
+                f.Close();
+            }
+            else if(cb_options.SelectedItem == "search for number")
+            {
+                int key;
+                try
+                {
+                    if (arr.Length < 1) ;
+                    bool b = Int32.TryParse(txt_input.Text, out key);
+                    if (b == false)
+                    {
+                        MessageBox.Show("please enter only numbers");
+                    }
+                    else
+                    {
+                        var time = System.Diagnostics.Stopwatch.StartNew();
+
+                        txt_show.Text = binary_search(key, arr).ToString() + "                time binary search function take it is :  ";
+
+                        time.Stop();
+                        txt_show.Text += time.Elapsed.TotalMilliseconds.ToString();
+                    }
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("run only in sorted file");
+                }
+                
+            }
+            else if (cb_options.SelectedItem == "merge sort file")
+            {
+                try
+                {
+                    open();
+                    var time = System.Diagnostics.Stopwatch.StartNew();
+
+                    arr = merge_sort(arr);
+
+                    time.Stop();
+
+                    txt_show.Text = "time merg sort function take it is :  " + time.Elapsed.TotalMilliseconds.ToString();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("error");
+                }
+                
+            }
+            else if (cb_options.SelectedItem == "quik sort file")
+            {
+                try
+                {
+                    open();
+                    var time = System.Diagnostics.Stopwatch.StartNew();
+
+                    arr = quick_sort(arr, 0, (arr.Length) - 1);
+
+                    time.Stop();
+
+                    txt_show.Text = "time quik sort function take it is :  " + time.Elapsed.TotalMilliseconds.ToString();
+                }
+                catch(Exception x)
+                {
+                    MessageBox.Show("error");
+                }
+                
+
+            }
+            else if (cb_options.SelectedItem == "heap sort file")
+            {
+                try
+                {
+                    open();
+                    var time = System.Diagnostics.Stopwatch.StartNew();
+                    
+                    arr = heap_sort(arr);
+
+                    time.Stop();
+
+                    txt_show.Text = "time heap sort function take it is :  " + time.Elapsed.TotalMilliseconds.ToString();
+                }
+                catch(Exception x)
+                {
+                    MessageBox.Show("error");
+                }
+            }
+            else if (cb_options.SelectedItem == "counting sort")
+            {
+                try
+                {
+                    open();
+                    var time = System.Diagnostics.Stopwatch.StartNew();
+
+                    arr = count_sort(arr);
+
+                    time.Stop();
+                    
+
+                    txt_show.Text = "time count sort function take it is :  " + time.Elapsed.TotalMilliseconds.ToString();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("error");
+                }
+
+            }
+        }
+        public void open()
+        {
+            try
+            {
+                StreamReader file = new StreamReader(txt_file.Text);
+                string s = file.ReadToEnd();
+                string[] split = s.Split(' ');
+                arr = new int[split.Length];
+                int x;
+                for (int i = 0; i < split.Length; i++)
+                {
+                    if (Int32.TryParse(split[i], out x))
+                    {
+                        arr[i] = x;
+                    }
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("enter valid file path");
+            }
+        }
+
+        public int binary_search(int key, int[] arr)
+        {
+            int start = 0, end = arr.Length-1 ;
+            int m =(start+end)/ 2;
+            while (end >= start)
+            {
+                if (key == arr[m])
+                    return m;
+                else if (key > arr[m])
+                {
+                    start = m+1;
+                    m = (start + end) / 2;
+                   
+                }
+                else if (key < arr[m])
+                {
+                    end = m-1;
+                    m = (start + end) / 2;
+                }
+            }
+            return -1;
+        }
+
+        public int[] merge_sort(int []arr)
+        {
+            if (arr.Length  == 1)
+                   return arr;
+           else if(arr.Length >1)
+            {
+                int e,a,f;
+                int[] arr1 = new int[(arr.Length ) / 2];
+                for (e =0;e< (arr.Length ) / 2;e++)
+                {
+                    arr1[e] = arr[e];
+                }
+                if((arr.Length) % 2  == 1)
+                    a = ((arr.Length ) / 2) + 1;
+                else
+                    a= (arr.Length ) / 2;
+                int[] arr2 = new int[a];
+
+                for (e= (arr.Length ) / 2,f=0; e < (arr.Length); f++,e++)
+                {
+                    arr2[f] = arr[e];
+                }
+                arr1 = merge_sort(arr1);
+                arr2 = merge_sort(arr2);
+
+                int i =0 ,j = 0,x=0;
+                while (i!=arr1.Length && j !=arr2.Length)
+                {
+                    if (arr1[i] >= arr2[j])
+                    {
+                        arr[x] = arr2[j];
+                        if(j<arr2.Length)
+                            j++;
+                    }
+                      
+                    else
+                    {
+                        arr[x] = arr1[i];
+                        if (i<arr1.Length)
+                            i++;
+                    }
+                    x++;
+                }
+                if (i == arr1.Length)
+                {
+                    while (j != arr2.Length)
+                    {
+                        arr[x] = arr2[j];
+                        x++;j++;
+                    }
+                       
+                }
+                else if(j == arr2.Length)
+                {
+                    while (i != arr1.Length)
+                    {
+                        arr[x] = arr1[i];
+                        i++;x++;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public int[] quick_sort(int []arr,int p ,int r)
+        {
+            int q = 0;
+            if (p < r)
+            {
+                q = partition(arr, p, r);
+                quick_sort(arr, p, q - 1);
+                quick_sort(arr, q + 1, r);
+            }
+            return arr;
+        }
+        public int partition(int[] arr, int p, int r)
+        {
+            int pivot = arr[p];
+            while (true)
+            {
+                while (arr[p] < pivot)
+                {
+                    p++;
+                }
+                while (arr[r] > pivot)
+                {
+                    r--;
+                }
+                if (p < r)
+                {
+                    swap(arr, p, r);
+                }
+                else
+                {
+                    return r;
+                }
+            }
+        }
+        
+        public int[] heap_sort(int[] arr)
+        {
+            int heapSize = arr.Length;
+
+            for (int p = (heapSize - 1) / 2; p >= 0; p--)
+                max_heap(arr, heapSize, p);
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                swap(arr, i, 0);
+
+                heapSize--;
+                max_heap(arr, heapSize, 0);
+            }
+            return arr;
+        }
+
+        private static void max_heap(int[] arr, int heapSize, int index)
+        {
+            int left = index *2;
+            int right = (index * 2) + 1;
+
+            int largest = 0;
+
+            if (left < heapSize && arr[left] > arr[index])
+                largest = left;
+            else
+                largest = index;
+
+            if (right < heapSize && arr[right] > arr[largest])
+                largest = right;
+
+            if (largest != index)
+            {
+                int temp = arr[index];
+                arr[index] = arr[largest];
+                arr[largest] = temp;
+
+                max_heap(arr, heapSize, largest);
+            }
+        }
+        public int[] count_sort(int []arr)
+        {
+            int [] A = new int[arr.Max()+1];
+            int[] B = new int[arr.Length];
+            for (int i = 0; i < A.Length; i++)
+                A[i] = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                    A[arr[i]]++;
+            }
+            for (int i = 1; i < A.Length; i++)
+                A[i] += A[i-1];
+            for (int i = arr.Length - 1; i >= 0; i--)
+                B[--A[arr[i]]] = arr[i];
+            return B;
+        }
+        public void swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "";
+                StreamWriter file = new StreamWriter(txt_output_path.Text);
+                for(int i = 0;i<arr.Length;i++)
+                {
+                    if (i % 20 != 0)
+                        str += arr[i] + "  ";
+                    else
+                        str += Environment.NewLine;
+                }
+                file.Write(str);
+                file.Close();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("enter valid file path");
+            }
+        }
+    }
+}
